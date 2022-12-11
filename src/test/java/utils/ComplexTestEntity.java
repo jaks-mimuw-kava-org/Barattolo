e96033d7c9635175e0b26a999cede37423e82c79
questras <michal.wisniewski422@gmail.com>
2022-12-11 10:17:37 +0100

    Add column annotation to tests.

--- src/test/java/utils/ComplexTestEntity.java
+++ src/test/java/utils/ComplexTestEntity.java
@@ -1,5 +1,6 @@
 package utils;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
@@ -9,6 +10,7 @@ import java.sql.Date;
 public class ComplexTestEntity {
     @Id
     private String name;
+    @Column(name = "last")
     private String lastName;
     private Long height;
     private int age;
--- src/test/resources/init_postgres.sql
+++ src/test/resources/init_postgres.sql
@@ -5,7 +5,7 @@ name varchar(50)
 
 CREATE TABLE ComplexTestEntity(
 name varchar(255) PRIMARY KEY,
-lastName varchar(255),
+last varchar(255),
 height BIGINT,
 age INT,
 lastLogin DATE
